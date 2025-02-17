c9f033d1470c27e04eea97077689faca1d55af25
Yogesh Shrestha <iamyogess@gmail.com>
2025-02-17 12:18:31 +0545

    active nav bar

--- components/FeaturedSection.tsx
+++ components/FeaturedSection.tsx
@@ -1,87 +1,87 @@
-  import React from "react";
-  import Image from "next/image";
+import React from "react";
+import Image from "next/image";
 
-  const FeaturedSection = () => {
-    return (
-      <section className="min-h-screen">
-        <div className="min-h-screen w-full">
-          {/* First Section */}
-          <div className="flex flex-col-reverse md:flex-row h-screen">
-            <div className="w-full md:w-1/2 h-1/2 md:h-full">
-              <Image
-                src="https://images.unsplash.com/photo-1620641606872-f9641b65adab?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8aW5kaWFuJTIwY2hlZnxlbnwwfHwwfHx8MA%3D%3D"
-                alt="Professional chef"
-                width={600}
-                height={800}
-                className="w-full h-full object-cover"
-              />
-            </div>
-            <div className="relative w-full md:w-1/2 flex flex-col justify-center items-start p-8 md:p-20">
-              <div className="absolute inset-0 w-full h-full">
-                <Image
-                  src="/reservation.jpg"
-                  alt="Background"
-                  layout="fill"
-                  objectFit="cover"
-                  className="z-0"
-                />
-                <div className="absolute inset-0 bg-black/80"></div>
-              </div>
-              <div className="relative z-10 flex flex-col gap-y-3">
-                <h1 className="text-3xl md:text-4xl font-bold text-yellow-300">
-                  (Reservation)
-                </h1>
-                <h1 className="text-3xl md:text-4xl font-bold text-white">
-                  Events & Functions
-                </h1>
-                <div className="bg-yellow-300 h-2 w-24"></div>
-                <p className="text-lg md:text-xl text-white">
-                  We accept reservations for Private parties, Group Events,
-                  Catering, Birthday Celebrations, Family Dining, or Group Dining
-                </p>
-              </div>
-            </div>
+const FeaturedSection = () => {
+  return (
+    <section className="min-h-screen">
+      <div className="min-h-screen w-full">
+        {/* First Section */}
+        <div className="flex flex-col-reverse md:flex-row h-screen">
+          <div className="w-full md:w-1/2 h-1/2 md:h-full">
+            <Image
+              src="https://images.unsplash.com/photo-1620641606872-f9641b65adab?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8aW5kaWFuJTIwY2hlZnxlbnwwfHwwfHx8MA%3D%3D"
+              alt="Professional chef"
+              width={600}
+              height={800}
+              className="w-full h-full object-cover"
+            />
           </div>
-
-          {/* Second Section */}
-          <div className="flex flex-col md:flex-row h-screen">
-            <div className="relative w-full md:w-1/2 flex flex-col justify-center items-center p-8 md:p-20">
-              <div className="absolute inset-0 w-full h-full">
-                <Image
-                  src="/hero.jpg"
-                  alt="Background"
-                  layout="fill"
-                  objectFit="cover"
-                  className="z-0"
-                />
-                <div className="absolute inset-0 bg-black/80"></div>
-              </div>
-              <div className="relative z-10 flex flex-col gap-y-3">
-                <h1 className="text-3xl md:text-4xl font-bold text-yellow-300"></h1>
-                <h1 className="text-3xl md:text-4xl font-bold text-white">
-                  Authentic Indian & Nepali Cuisines
-                </h1>
-                <div className="bg-yellow-300 h-2 w-24"></div>
-                <p className="text-lg md:text-xl text-white">
-                  Experience the rich flavors of Indian and Nepali cuisine,
-                  perfect for private parties, group events, catering, and family
-                  gatherings.
-                </p>
-              </div>
-            </div>
-            <div className="w-full md:w-1/2 h-1/2 md:h-full">
+          <div className="relative w-full md:w-1/2 flex flex-col justify-center items-start p-8 md:p-20">
+            <div className="absolute inset-0 w-full h-full">
               <Image
-                src="/foodtable.jpg"
-                alt="Professional chef"
-                width={600}
-                height={800}
-                className="w-full h-full object-cover"
+                src="/reservation.jpg"
+                alt="Background"
+                layout="fill"
+                objectFit="cover"
+                className="z-0"
               />
+              <div className="absolute inset-0 bg-black/80"></div>
+            </div>
+            <div className="relative z-10 flex flex-col gap-y-3">
+              <h1 className="text-3xl md:text-4xl font-bold text-yellow-300">
+                (Reservation)
+              </h1>
+              <h1 className="text-3xl md:text-4xl font-bold text-white">
+                Events & Functions
+              </h1>
+              <div className="bg-yellow-300 h-2 w-24"></div>
+              <p className="text-lg md:text-xl text-white">
+                We accept reservations for Private parties, Group Events,
+                Catering, Birthday Celebrations, Family Dining, or Group Dining
+              </p>
             </div>
           </div>
         </div>
-      </section>
-    );
-  };
 
-  export default FeaturedSection;
+        {/* Second Section */}
+        <div className="flex flex-col md:flex-row h-screen">
+          <div className="relative w-full md:w-1/2 flex flex-col justify-center items-center p-8 md:p-20">
+            <div className="absolute inset-0 w-full h-full">
+              <Image
+                src="/hero.jpg"
+                alt="Background"
+                layout="fill"
+                objectFit="cover"
+                className="z-0"
+              />
+              <div className="absolute inset-0 bg-black/80"></div>
+            </div>
+            <div className="relative z-10 flex flex-col gap-y-3">
+              <h1 className="text-3xl md:text-4xl font-bold text-yellow-300"></h1>
+              <h1 className="text-3xl md:text-4xl font-bold text-white">
+                Authentic Indian & Nepali Cuisines
+              </h1>
+              <div className="bg-yellow-300 h-2 w-24"></div>
+              <p className="text-lg md:text-xl text-white">
+                Experience the rich flavors of Indian and Nepali cuisine,
+                perfect for private parties, group events, catering, and family
+                gatherings.
+              </p>
+            </div>
+          </div>
+          <div className="w-full md:w-1/2 h-1/2 md:h-full">
+            <Image
+              src="/foodtable.jpg"
+              alt="Professional chef"
+              width={600}
+              height={800}
+              className="w-full h-full object-cover"
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default FeaturedSection;
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,12 +1,14 @@
-"use client"
+"use client";
 
 import { Menu, X } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React, { useState } from "react";
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const pathname = usePathname();
 
   return (
     <nav className="w-full bg-transparent absolute top-0 z-10 h-32 px-6 md:px-0">
@@ -15,56 +17,113 @@ const Navbar = () => {
           {/* Desktop Menu */}
           <ul className="hidden md:flex gap-x-10 text-xl">
             <li>
-              <Link href="/">Home</Link>
+              <Link
+                href="/"
+                className={`${pathname == "/" ? "text-[#d4b78f]" : ""}`}
+              >
+                Home
+              </Link>
             </li>
             <li>
-              <Link href="/menu">Menu</Link>
+              <Link
+                href="/menu"
+                className={`${pathname == "/menu" ? "text-[#d4b78f]" : ""}`}
+              >
+                Menu
+              </Link>
             </li>
           </ul>
-          
+
           {/* Logo */}
           <a href="/">
-            <Image src="/rajdootlogo.png" height={150} width={150} alt="Rajdoot Logo" />
+            <Image
+              src="/rajdootlogo.png"
+              height={150}
+              width={150}
+              alt="Rajdoot Logo"
+            />
           </a>
-          
+
           {/* Desktop Menu */}
           <ul className="hidden md:flex gap-x-10 text-xl">
             <li>
-              <Link href="/about">About</Link>
+              <Link
+                href="/about"
+                className={`${pathname == "/about" ? "text-[#d4b78f]" : ""}`}
+              >
+                About
+              </Link>
             </li>
             <li>
-              <Link href="/gallery">Gallery</Link>
+              <Link
+                href="/gallery"
+                className={`${pathname == "/gallery" ? "text-[#d4b78f]" : ""}`}
+              >
+                Gallery
+              </Link>
             </li>
           </ul>
-          
+
           {/* Mobile Menu Button */}
           <div className="md:hidden" onClick={() => setIsOpen(!isOpen)}>
-            {isOpen ? <X className="h-8 w-8"/> : <Menu />}
+            {isOpen ? <X className="h-8 w-8" /> : <Menu />}
           </div>
         </div>
 
         {/* Mobile Menu */}
         <div
-          className={`fixed z-40 top-0 right-0 h-full w-64 bg-[#faf7f2] text-black p-6 transform transition-transform duration-300 ease-in-out ${isOpen ? "translate-x-0" : "translate-x-full"}`}
+          className={`fixed z-40 top-0 right-0 h-full w-64 bg-[#faf7f2] text-black p-6 transform transition-transform duration-300 ease-in-out ${
+            isOpen ? "translate-x-0" : "translate-x-full"
+          }`}
         >
           <div className="flex justify-end">
             <X onClick={() => setIsOpen(false)} />
           </div>
           <ul className="flex flex-col gap-y-4 text-2xl mt-6">
             <li>
-              <Link href="/" onClick={() => setIsOpen(false)}>Home</Link>
+              <Link
+                href="/"
+                onClick={() => setIsOpen(false)}
+                className={`${pathname == "/" ? "text-[#d4b78f]" : ""}`}
+              >
+                Home
+              </Link>
             </li>
             <li>
-              <Link href="/menu" onClick={() => setIsOpen(false)}>Menu</Link>
+              <Link
+                href="/menu"
+                onClick={() => setIsOpen(false)}
+                className={`${pathname == "/menu" ? "text-[#d4b78f]" : ""}`}
+              >
+                Menu
+              </Link>
             </li>
             <li>
-              <Link href="/about" onClick={() => setIsOpen(false)}>About</Link>
+              <Link
+                href="/about"
+                onClick={() => setIsOpen(false)}
+                className={`${pathname == "/about" ? "text-[#d4b78f]" : ""}`}
+              >
+                About
+              </Link>
             </li>
             <li>
-              <Link href="/contact" onClick={() => setIsOpen(false)}>Contact</Link>
+              <Link
+                href="/contact"
+                onClick={() => setIsOpen(false)}
+                className={`${pathname == "/contact" ? "text-[#d4b78f]" : ""}`}
+              >
+                Contact
+              </Link>
             </li>
             <li>
-              <Link href="/gallery" onClick={() => setIsOpen(false)}>Gallery</Link>
+              <Link
+                href="/gallery"
+                onClick={() => setIsOpen(false)}
+                className={`${pathname == "/gallery" ? "text-[#d4b78f]" : ""}`}
+              >
+                Gallery
+              </Link>
             </li>
           </ul>
         </div>
